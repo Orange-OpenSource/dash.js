c05bc0ef80ba6cac390174fbe16fdaf0770983e6
Gontran Baerts <gbaerts@molotov.tv>
2017-09-26 14:34:42 +0200

    Make xml2json a true ES6 class + optimized management of repeated segments in MPD files
    Signed-off-by: Gontran Baerts <gbaerts@molotov.tv>

--- externals/molotovtv/xml2json.js
+++ externals/molotovtv/xml2json.js
@@ -26,35 +26,33 @@
 */
 
 const emptyTextNodeOrComment = /\>\s+(<!--.+--\>\s+)?</g;
+const segmentRepeat = /(<S d="(\d+)" \/><S d="(\d+)" \/><S d="(\d+)" \/>)+/;
+const DOMNodeTypes = {
+    ELEMENT_NODE: 1,
+    TEXT_NODE: 3,
+    CDATA_SECTION_NODE: 4,
+    COMMENT_NODE: 8,
+    DOCUMENT_NODE: 9
+};
+const VERSION = '1.2.0';
 
-function X2JS(config) {
-    'use strict';
+export default class X2JS {
 
-    var VERSION = '1.2.0';
 
-    config = config || {};
+    constructor(config) {
 
-    const DOMNodeTypes = {
-        ELEMENT_NODE: 1,
-        TEXT_NODE: 3,
-        CDATA_SECTION_NODE: 4,
-        COMMENT_NODE: 8,
-        DOCUMENT_NODE: 9
-    };
+        this.config = config || {};
 
-    initConfigDefaults();
-
-    function initConfigDefaults() {
         if (config.ignoreRoot === undefined) {
-            config.ignoreRoot = false;
+            this.config.ignoreRoot = false;
         }
     }
 
-    function toArrayAccessForm(obj, childName) {
+    toArrayAccessForm(obj, childName) {
         obj[childName + '_asArray'] = obj[childName] instanceof Array ? obj[childName] : [obj[childName]];
     }
 
-    function parseDOMChildren(node) {
+    parseDOMChildren(node) {
 
         let result;
 
@@ -62,7 +60,7 @@ function X2JS(config) {
 
             case DOMNodeTypes.DOCUMENT_NODE:
 
-                return config.ignoreRoot ? parseDOMChildren(node.firstChild) : { [node.firstChild.nodeName]: parseDOMChildren(node.firstChild) };
+                return this.config.ignoreRoot ? this.parseDOMChildren(node.firstChild) : { [node.firstChild.nodeName]: this.parseDOMChildren(node.firstChild) };
 
             case DOMNodeTypes.ELEMENT_NODE:
 
@@ -72,30 +70,60 @@ function X2JS(config) {
                 let child = node.firstChild;
                 if (child) {
 
-                    const len = node.childNodes.length;
+                    let len = node.childNodes.length;
                     let children = new Array(len);
 
                     for (let cidx = 0; cidx < len; ++cidx) {
 
                         let childName = child.nodeName;
 
-                        let childNode = result[childName];
-                        if (!childNode) {
-                            let c = parseDOMChildren(child);
-                            children[cidx] = { [childName]: c };
-                            result[childName] = c;
-                            toArrayAccessForm(result, childName);
-                        }
-                        else {
-                            if (childNode && !(childNode instanceof Array)) {
-                                childNode = result[childName] = [result[childName]];
-                                toArrayAccessForm(result, childName);
+                        if (childName === 'repeat') {
+
+                            // number of segments to add
+                            let count = parseInt(child.attributes.count.value, 10) * 3;
+                            len += count - 1;
+
+                            let segmentList;
+                            segmentList = new Array(count + 1);
+                            segmentList[0] = result.S;
+                            result.S = result.S_asArray = segmentList;
+
+                            // starting at index 1 for the segmentList array
+                            let index = 1;
+                            while (index < count) {
+                                segmentList[index++] = this.segment1;
+                                segmentList[index++] = this.segment2;
+                                segmentList[index++] = this.segment3;
+                                children[cidx++] = this.child1;
+                                children[cidx++] = this.child2;
+                                children[cidx++] = this.child3;
                             }
 
-                            let c = parseDOMChildren(child);
-                            children[cidx] = { [childName]: c };
-                            childNode[result[childName].length] = c;
+                            --cidx; // have to go backward because cidx will be incremented in the for loop
+
+                        } else {
+
+                            let childNode = result[childName];
+                            if (!childNode) {
+                                let c = this.parseDOMChildren(child);
+                                children[cidx] = { [childName]: c };
+                                result[childName] = c;
+                                // FIXME: change this call by direct code
+                                this.toArrayAccessForm(result, childName);
+                            }
+                            else {
+                                if (childNode && !(childNode instanceof Array)) {
+                                    childNode = result[childName] = [result[childName]];
+                                    // FIXME: change this call by direct code
+                                    this.toArrayAccessForm(result, childName);
+                                }
+
+                                let c = this.parseDOMChildren(child);
+                                children[cidx] = { [childName]: c };
+                                childNode[childNode.length] = c;
+                            }
                         }
+
                         child = child.nextSibling;
                     }
 
@@ -110,8 +138,8 @@ function X2JS(config) {
                     let attr = nodeAttributes[aidx];
 
                     let value = attr.value;
-                    for (let m = 0, ml = config.matchers.length; m < ml; ++m) {
-                        let matchobj = config.matchers[m];
+                    for (let m = 0, ml = this.config.matchers.length; m < ml; ++m) {
+                        let matchobj = this.config.matchers[m];
                         if (matchobj.test(attr, nodeLocalName)) {
                             value = matchobj.converter(attr.value);
                         }
@@ -135,7 +163,7 @@ function X2JS(config) {
         }
     }
 
-    this.parseXmlString = function (xmlDocStr) {
+    parseXmlString(xmlDocStr) {
 
         if (xmlDocStr === undefined) { return null; }
 
@@ -145,7 +173,7 @@ function X2JS(config) {
 
         try {
             xmlDoc = parser.parseFromString( xmlDocStr, 'text/xml' );
-            if ( parsererrorNS != null && xmlDoc.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0) {
+            if ( parsererrorNS && xmlDoc.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0) {
                 xmlDoc = null;
             }
         }
@@ -154,20 +182,34 @@ function X2JS(config) {
         }
 
         return xmlDoc;
-    };
+    }
 
-    this.xml_str2json = function (xmlDocStr) {
+    xml_str2json(xmlDocStr) {
         // The regex:
         // - removes sequences of space-like characters between elements (including newlines)
         // - removes XML comments
         // - replaces sequences of space-like characters by a single space
-        var xmlDoc = this.parseXmlString(xmlDocStr.replace(emptyTextNodeOrComment, '><').replace(/\s+/g, ' '));
-        return xmlDoc ? parseDOMChildren(xmlDoc) : null;
-    };
+        let xmlDocStrClean = xmlDocStr.replace(emptyTextNodeOrComment, '><').replace(/\s+/g, ' ');
 
-    this.getVersion = function () {
+        let segmentMatches = xmlDocStrClean.match(segmentRepeat);
+        if (segmentMatches) {
+
+            this.segment1 = {d: parseFloat(segmentMatches[2])};
+            this.segment2 = {d: parseFloat(segmentMatches[3])};
+            this.segment3 = {d: parseFloat(segmentMatches[4])};
+            this.child1 = {S: this.segment1};
+            this.child2 = {S: this.segment2};
+            this.child3 = {S: this.segment3};
+
+            const match = segmentMatches[0];
+            xmlDocStrClean = xmlDocStrClean.split(match).join(`<repeat count="${match.length / segmentMatches[1].length}"/>`);
+        }
+
+        const xmlDoc = this.parseXmlString(xmlDocStrClean);
+        return xmlDoc ? this.parseDOMChildren(xmlDoc) : null;
+    }
+
+    getVersion() {
         return VERSION;
-    };
+    }
 }
-
-export default X2JS;
